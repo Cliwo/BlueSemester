2a9156eb7c269696cd7420b8b842ba740a58b21b
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-07-26 00:18:15 +0900

    Initial Project

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    public static CameraManager instance;
+
+    public GameObject character;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnScroll(float delta)
+    {
+        Vector3 dir = character.transform.position - transform.position;
+        transform.Translate(dir * delta);
+    }
+
+    public void OnDrag(float delta)
+    {
+        
+    }
+
+}
