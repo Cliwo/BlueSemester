442e8bad8bd28283fe40c54e94e074518b47d9b2
Seungchan Jeong cliwo@naver.com <cliwo@naver.com>
2018-07-26 15:44:13 +0900

    Scrolling Feature

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour {
 
     public static CameraManager instance;
-
     public GameObject character;
+    public float minDistance;
+    public float maxDistance;
 
     private void Awake()
     {
@@ -27,13 +28,30 @@ public class CameraManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		OnScroll(Input.mouseScrollDelta.y);
+        Debug.Log(Input.mouseScrollDelta);
 	}
 
     public void OnScroll(float delta)
     {
-        Vector3 dir = character.transform.position - transform.position;
-        transform.Translate(dir * delta);
+        Vector3 dis = character.transform.position - transform.position;
+        float currentSqrMag = dis.sqrMagnitude;
+        if(currentSqrMag < minDistance*minDistance && delta > 0 || currentSqrMag > maxDistance*maxDistance && delta < 0)
+        {
+            return;
+        }
+        if((transform.position + dis.normalized * delta).sqrMagnitude < minDistance*minDistance)
+        {
+            transform.position = character.transform.position + -dis.normalized * minDistance;
+        }
+        else if((transform.position + dis.normalized * delta).sqrMagnitude > maxDistance*maxDistance)
+        {
+            transform.position = character.transform.position + -dis.normalized * maxDistance;
+        }
+        else 
+        {
+            transform.Translate(dis.normalized * delta);
+        }
     }
 
     public void OnDrag(float delta)
