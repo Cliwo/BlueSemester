0326c585499152c9379021ef97400b5204dc49c5
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-08-07 19:49:57 +0900

    Pin-Point-Walking is now available but needed to be fixed. Not Done Yet

--- Assets/Scripts/MinimapManager.cs
+++ Assets/Scripts/MinimapManager.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MinimapManager : MonoBehaviour {
-    public GameObject target;
+public class MinimapManager : MonoBehaviour
+{
+    public GameObject target; //for now this is character
+
     public static MinimapManager instance;
+
+    private Vector3 dir;
+    private bool shouldMove = false;
+
     private void Awake()
     {
         if (instance == null)
@@ -26,5 +34,20 @@ public class MinimapManager : MonoBehaviour {
         transform.rotation = Quaternion.Euler(90.0f, target.transform.eulerAngles.y, 0.0f);
     }
 
+    private void Update()
+    {
+        if(shouldMove)
+        {
+            target.transform.Translate(dir);
+        }
+    }
+
+    public void OnMinimapPinEneabled(Vector2 normalizedDirection)
+    {
+        dir = new Vector3(normalizedDirection.x, 0, normalizedDirection.y);
+        shouldMove = true;
+    }
+
+   
 
 }
---
+++ Assets/Scripts/MinimapUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MinimapUI : MonoBehaviour, IPointerClickHandler{
+
+    public RectTransform pinPointImage;
+    [HideInInspector]
+    public RawImage pinImage;
+
+    private void Awake()
+    {
+        pinImage = pinPointImage.GetComponent<RawImage>();
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        pinPointImage.localPosition = new Vector3(eventData.position.x - Screen.width / 2, eventData.position.y - Screen.height / 2, 0);
+        ShowPin();
+
+        RectTransform rectTrans = GetComponent<RectTransform>();
+        //https://answers.unity.com/questions/1013011/convert-recttransform-rect-to-screen-space.html
+        Vector2 size = Vector2.Scale(rectTrans.rect.size, rectTrans.lossyScale);
+        Vector2 normalizedDir = (new Vector2(pinPointImage.position.x, pinPointImage.position.y) - new Vector2(rectTrans.position.x, rectTrans.position.y)) / size;
+        Debug.Log(normalizedDir);
+        MinimapManager.instance.OnMinimapPinEneabled(normalizedDir);
+    }
+
+    public void ShowPin()
+    {
+        pinImage.enabled = true;
+    }
+
+    public void HidePin()
+    {
+        pinImage.enabled = false;
+    }
+}
