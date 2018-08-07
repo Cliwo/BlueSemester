e66edab406c39a2331363a3e68558280e47b6da2
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-08-07 16:04:18 +0900

    Minimap Basic feature 1. Render minimap at top-right 2. Cast Shadow even if it is texture

---
+++ Assets/Scripts/MinimapManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapManager : MonoBehaviour {
+    public GameObject target;
+    public static MinimapManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = target.transform.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(90.0f, target.transform.eulerAngles.y, 0.0f);
+    }
+
+
+}
---
+++ Assets/Scripts/TestScripts/BasicMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        vert *= Time.deltaTime * 10.0f;
+
+        transform.Translate(new Vector3(0, 0, vert));
+        transform.Rotate(new Vector3(0, horiz, 0));
+    }
+}
