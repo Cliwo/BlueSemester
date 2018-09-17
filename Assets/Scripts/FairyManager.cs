51feb62b732a454ab3cf389bbfe426558447de2d
cocococco <kyeonsoo26@gmail.com>
2018-09-17 12:45:27 +0900

    요정이 플레이어 따라감

---
+++ Assets/Scripts/FairyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private float xDistance;
+
+    [SerializeField]
+    private float yDistance;
+
+    [SerializeField]
+    private float zDistance;
+
+    [SerializeField]
+    public float dampSpeed;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 newPos = target.position + new Vector3(xDistance, yDistance, -zDistance);
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * dampSpeed);
+        transform.LookAt(target);
+    }
+}
