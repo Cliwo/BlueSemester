fba930c84ea95cdd260ae13b1a6773d1d146eaf4
Chan <cliwo@naver.com>
2018-09-22 18:19:51 +0900

    Testing Cursor

---
+++ Assets/CursorTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorTest : MonoBehaviour {
+
+	public Texture2D cursorImage;
+	public Texture2D cursorImageExit;
+	private void OnMouseEnter() {
+		Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+	private void OnMouseExit() {
+		Cursor.SetCursor(cursorImageExit, Vector2.zero, CursorMode.ForceSoftware);	
+	}
+	
+}
