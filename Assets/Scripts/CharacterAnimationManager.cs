5a1591d4d38bc3d873084a997a0ae0a20ecd26f0
Chan <cliwo@naver.com>
2018-09-22 19:22:21 +0900

    1. Animation Controller Setting 2. Add Animation Manager

---
+++ Assets/Scripts/CharacterAnimationManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationManager : MonoBehaviour {
+
+	public Animator animator;
+	private static CharacterAnimationManager instance;
+	public static CharacterAnimationManager getInstance()
+	{
+		return instance;
+	}
+
+	void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this)
+        {
+            DestroyImmediate(this);
+        }
+        DontDestroyOnLoad(this);
+    }
+	
+	public void TriggerAnimator(string trigger)
+	{
+		animator.SetTrigger(trigger);
+	}
+
+	public class AnimatorTrigger
+	{
+		public const string Punch = "Punch";
+		public const string Idle = "Idle";
+		public const string Walking = "Walk";
+
+	}
+}
