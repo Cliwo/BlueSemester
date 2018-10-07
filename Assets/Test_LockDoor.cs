c54b5b35e9209bf3dee485cb62f2e07f42108987
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-10-07 20:57:19 +0900

    Door Down (Unlock)

--- Assets/Test_LockDoor.cs
+++ Assets/Test_LockDoor.cs
@@ -7,24 +7,13 @@ public class Test_LockDoor : MonoBehaviour {
 	public GameObject door;
 	Animator animator;
 
-	/// <summary>
-	/// Awake is called when the script instance is being loaded.
-	/// </summary>
 	void Awake()
 	{
 		animator = door.GetComponent<Animator>();
 	}
-	/// <summary>
-	/// OnTriggerEnter is called when the Collider other enters the trigger.
-	/// </summary>
-	/// <param name="other">The other Collider involved in this collision.</param>
-	void OnTriggerEnter(Collider other)
-	{
-		Debug.Log("Enter");
-	}
+
 	void OnTriggerExit(Collider other)
 	{
-		Debug.Log("Exit");
 		if(other.tag == "Player")
 		{
 			animator.SetTrigger("LockDoor");
