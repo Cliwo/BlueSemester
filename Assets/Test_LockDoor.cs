e418278bca177c6d1ca28ce4f7cdbb1c5bac4e27
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-10-07 20:37:27 +0900

    Sample Door Anim _ Lock

---
+++ Assets/Test_LockDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_LockDoor : MonoBehaviour {
+	
+	public GameObject door;
+	Animator animator;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		animator = door.GetComponent<Animator>();
+	}
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Enter");
+	}
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Exit");
+		if(other.tag == "Player")
+		{
+			animator.SetTrigger("LockDoor");
+		}
+	}
+}
