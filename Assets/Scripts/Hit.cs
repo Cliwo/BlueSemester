06cb98ee23c9a1cdee0eb88947eb927a7da67e3b
cocococco <kyeonsoo26@gmail.com>
2018-10-21 04:12:01 +0900

    플레이어 기본공격 누락된거

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    private bool isHit;
+
+    private void Start()
+    {
+        isHit = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy Hit!!!!!!!!!!!!!!!");
+            isHit = true;
+        }
+    }
+}
