e6d862ce392f4ea77c12b4ba05a9455d687d57c9
Chan <cliwo@naver.com>
2018-10-31 19:34:58 +0900

    리소스 랜덤 생성 중간 저장 (아래의 기능 작동) 1. 랜덤 scale 적용 2. y 좌표 자동 조정 (메쉬의 크기에 맞게) 3. SpawnArea의 children으로 생성된 Resource 들이 들어가도록 적용 (유니티 엔진상 버그 존재, 버그 우회)

--- Assets/SpawnArea.cs
+++ Assets/SpawnArea.cs
@@ -6,14 +6,15 @@ public class SpawnArea : MonoBehaviour {
 
 	//TODO : 10월 30일 modeling load하는거 비동기적으로 하는 코드 만들어야 할 수 도 있음.
 	//COMMENT : 성능상의 향상을 원하면 큰 SpawnArea를 정의하지 않고, 큰 Area를 작게 쪼개서 만드는게 알고리즘상 유리함
-	// n제곱의 알고리즘에서 n을 크게만드는 것 보다 작은 n의 제곱을 더하는게 유리하기 때문
-
+	//          n제곱의 알고리즘에서 n을 크게만드는 것 보다 작은 n의 제곱을 더하는게 유리하기 때문
+	public float maximumScale; /* 자원 최대 크기 */
+	public float minimumScale; /* 자원 최소 크기 */
 	public int MaximumCount; /* 최대 자원 갯수 */
 	public int GeneratingCountAtOnce; /* 한 번에 생성 갯수 */
 	public float UpdateInterval; /* 업데이트 주기 */
 
-	[HideInInspector]
-	public List<GameObject> Resources;
+	private List<GameObject> InstantiatedResources;
+	private List<Collider> OccupiedArea;
 	public GameObject Resource; //Manager가 생기면 옮길 수도
 	public SpawnObjectKind kind; //Manager가 생기면 옮길 수도
 	public enum SpawnObjectKind //Manager가 생기면 옮길 수도
@@ -21,13 +22,17 @@ public class SpawnArea : MonoBehaviour {
 		TREE, SHELL, IRON_ORE, SULFUR,
 	}
 
-	float timeBucket = float.MinValue;
 	float minWorldX;
 	float maxWorldX;
 	float minWorldZ;
 	float maxWorldZ;
+	float timeBucket = float.MinValue;
+	const float raycastStartYPos = 100f;
 	void Start() 
 	{
+		InstantiatedResources = new List<GameObject>();
+		OccupiedArea = new List<Collider>();
+
 		Collider instantiateAvailableArea = GetComponent<Collider>(); 
 		GetComponent<MeshRenderer>().enabled = false;
 
@@ -38,53 +43,63 @@ public class SpawnArea : MonoBehaviour {
 
 		instantiateAvailableArea.enabled = false;
 	}
+
 	void Update()
 	{
 		if(Time.time > UpdateInterval + timeBucket)
 		{
 			timeBucket = Time.time;
-			if(Resources.Count == MaximumCount)
+			if(InstantiatedResources.Count == MaximumCount)
 			{
 				return;
 			}
-			int count = Random.Range(1, Mathf.Min(GeneratingCountAtOnce + 1, MaximumCount - Resources.Count));
-			for(int i = 0 ; i < count ; i++)
-			{	
-				Collider col = Resource.GetComponent<Collider>();
-				Vector3 newPos;
-				if(GetRandomValidPosition(out newPos, col.bounds.size))
-				{
-					GameObject instantiated = GameObject.Instantiate(Resource);
-					instantiated.gameObject.transform.position = newPos;
-					Resources.Add(instantiated);
-
-					instantiated.transform.parent = transform;
-					Collider instantiatedCol = instantiated.GetComponent<Collider>();
-					instantiatedCol.enabled = false;
-					instantiatedCol.enabled = true;
-				}
-				else
-				{	
-					//생성을 해야하는데 적당한 빈공간이 없는 경우 
-					return;
-				}
-			}
+			int count = Random.Range(1, Mathf.Min(GeneratingCountAtOnce + 1, MaximumCount - InstantiatedResources.Count));
+			InstantiateRandomResources(count);
 		}	
 	}
 	public void RemoveGameObject(GameObject g)
 	{
-		Resources.Remove(g);
+		OccupiedArea.Remove(g.GetComponent<Collider>());
+		InstantiatedResources.Remove(g);
 		DestroyImmediate(g);
 	}
 
-	bool GetRandomValidPosition(out Vector3 pos, Vector3 colSize, int tryCount = 1000)
+	private void InstantiateRandomResources(int count)
+	{
+		for(int i = 0 ; i < count ; i++)
+		{	
+			Collider col = Resource.GetComponent<Collider>();
+			Vector3 newPos;
+			float desiredSize = Random.Range(minimumScale, maximumScale);
+			if(GetRandomValidPosition(out newPos, col.bounds.size * desiredSize))
+			{
+				GameObject instantiated = GameObject.Instantiate(Resource);
+				instantiated.gameObject.transform.position = newPos;
+				instantiated.gameObject.transform.localScale *= desiredSize;
+				InstantiatedResources.Add(instantiated);
+
+				instantiated.transform.parent = transform;
+				
+				Collider instantiatedCol = instantiated.GetComponent<Collider>();
+				instantiatedCol.enabled = false; /* 18.10.31 currently this is for unity bug */
+				instantiatedCol.enabled = true;
+				OccupiedArea.Add(instantiatedCol);
+			}
+			else
+			{	
+				//생성을 해야하는데 적당한 빈공간이 없는 경우 
+				return;
+			}
+		}
+	}
+	private bool GetRandomValidPosition(out Vector3 pos, Vector3 colSize, int tryCount = 1000)
 	{
 		Vector3 copy;
 		int index = 0; 
 		do{
 			float newX = Random.Range(minWorldX, maxWorldX);
 			float newZ = Random.Range(minWorldZ, maxWorldZ);
-			pos = new Vector3(newX, 100f, newZ); //100 보다 float.MaxValue가 낫지 않을까 
+			pos = new Vector3(newX, raycastStartYPos, newZ);
 			RaycastHit hit;
 			if(Physics.Raycast(pos, Vector3.down, out hit))
 			{
@@ -93,23 +108,12 @@ public class SpawnArea : MonoBehaviour {
 			copy = pos;
 			index ++;
 
-			if(Resources.Count == 0)
+			if(InstantiatedResources.Count == 0)
 			{
 				break;
 			}
 		}
-		while(Resources.Any( (g) => {
-			Bounds testBounds = new Bounds(copy, colSize);
-			Collider exsistingCollider = g.GetComponent<Collider>();
-		
-			Bounds exsistingBounds = exsistingCollider.bounds;
-
-			Debug.DrawRay(exsistingBounds.center, Vector3.up * 3.0f, Color.cyan, 1.0f);
-			
-			bool temp = testBounds.Intersects(exsistingBounds);
-			return temp;
-			 }) && index < tryCount);
-		// 개선 가능 : 아에 Resources 의 bounds만 따로 저장하기
+		while(OccupiedArea.Any( (g) => new Bounds(copy, colSize).Intersects(g.bounds)) && index < tryCount);
 
 		if(index == tryCount)
 		{
@@ -117,8 +121,12 @@ public class SpawnArea : MonoBehaviour {
 			return false;
 		}
 		
-		Mesh mesh = Resource.GetComponent<MeshFilter>().mesh;
-		pos.y += mesh.bounds.extents.y * Resource.transform.localScale.y;
+		pos.y += GetMeshHalfHeight(Resource);
 		return true;
+	}
+
+	private float GetMeshHalfHeight(GameObject g)
+	{
+		return g.GetComponent<MeshFilter>().mesh.bounds.extents.y * g.transform.localScale.y;
 	}
 }
