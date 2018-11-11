0b8c66cf3bf51191d38b5f1606771990acc12835
cocococco <kyeonsoo26@gmail.com>
2018-11-12 02:57:13 +0900

    포세이돈 스크립트 따로 구현함 일단 다 range공격하는걸로

---
+++ Assets/Scripts/Enemy/RaidMonster.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaidMonster : MonoBehaviour
+{
+    private int currentSkill;
+    private int[] skillSet = new int[5];
+    // 1 : tornado
+    // 2 : thunderstroke
+    // 3 : wield
+    // 4 : summon
+    // 5 : pierce
+
+    private float maxHP = 500;
+    private float currentHP;
+    private Transform targetPlayer;
+    private CharacterManager inst_Character;
+
+    [SerializeField]
+    private GameObject rangedSpell;
+
+    private void Start()
+    {
+        inst_Character = CharacterManager.getInstance();
+        targetPlayer = inst_Character.GetComponent<Transform>();
+
+        skillSet[0] = 1;
+        skillSet[1] = 2;
+        skillSet[2] = 3;
+        skillSet[3] = 4;
+        skillSet[4] = 5;
+        currentHP = maxHP;
+        currentSkill = 0;
+
+        // 공격 시작 조건
+        StartCoroutine("StartPattern");
+    }
+
+    private IEnumerator StartPattern()
+    {
+        while (currentHP > 0)
+        {
+            UseSkill();
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+    private void UseSkill()
+    {
+        switch (skillSet[currentSkill])
+        {
+            case 1:
+                Tornado();
+                break;
+
+            case 2:
+                ThunderStroke();
+                break;
+
+            case 3:
+                Wield();
+                break;
+
+            case 4:
+                Summon();
+                break;
+
+            case 5:
+                Pierce();
+                break;
+        }
+        currentSkill++;
+        if (currentSkill >= skillSet.Length)
+        {
+            currentSkill = 0;
+        }
+    }
+
+    private void Tornado()
+    {
+        Debug.Log("Tornado");
+        GameObject clone = Instantiate(rangedSpell, this.transform.position, this.transform.rotation);
+        clone.GetComponent<RangedSpell>().target = targetPlayer;
+    }
+
+    private void ThunderStroke()
+    {
+        Debug.Log("ThunderStroke");
+        GameObject clone = Instantiate(rangedSpell, this.transform.position, this.transform.rotation);
+        clone.GetComponent<RangedSpell>().target = targetPlayer;
+    }
+
+    private void Wield()
+    {
+        Debug.Log("Wield");
+        GameObject clone = Instantiate(rangedSpell, this.transform.position, this.transform.rotation);
+        clone.GetComponent<RangedSpell>().target = targetPlayer;
+    }
+
+    private void Summon()
+    {
+        Debug.Log("Summon");
+        GameObject clone = Instantiate(rangedSpell, this.transform.position, this.transform.rotation);
+        clone.GetComponent<RangedSpell>().target = targetPlayer;
+    }
+
+    private void Pierce()
+    {
+        Debug.Log("Pierce");
+        GameObject clone = Instantiate(rangedSpell, this.transform.position, this.transform.rotation);
+        clone.GetComponent<RangedSpell>().target = targetPlayer;
+    }
+}
---
+++ Assets/Scripts/Enemy/RangedSpell.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedSpell : MonoBehaviour
+{
+    public Transform target;
+    private CharacterManager inst_Character;
+    private float lifespan = 5.0f;
+
+    private void Start()
+    {
+        inst_Character = CharacterManager.getInstance();
+        if (target == null)
+        {
+            target = inst_Character.GetComponent<Transform>();
+        }
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, target.position.z);
+        this.transform.LookAt(targetPos);
+
+        Destroy(this.gameObject, lifespan);
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            float distance = Vector3.Distance(target.position, this.transform.position);
+            if (distance > 2.0f)
+            {
+                transform.Translate(Vector3.forward * 8.0f * Time.deltaTime);
+            }
+            else
+            {
+                HitTarget();
+            }
+        }
+    }
+
+    private void HitTarget()
+    {
+        Destroy(this.gameObject);
+    }
+}
