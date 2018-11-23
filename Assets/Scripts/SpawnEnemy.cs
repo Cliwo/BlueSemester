242cb59f8b08572a1778828d9cdb9bf411a8a9bb
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-11-23 20:46:56 +0900

    in progress

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public List<SpawnLevel> levels;
+	private int currentLevel;
+
+	public event Action ConsumeEvents = () => { };
+	public event Action PersistEvents = ()=> { };
+	
+	public void AllCleared()
+	{
+		ConsumeEvents.Invoke();
+		PersistEvents.Invoke();
+
+		ConsumeEvents = () => {};
+	}
+
+	void Update()
+	{
+		if(levels[currentLevel].IsClear())
+		{
+			if(currentLevel == levels.Count -1 )
+			{
+				AllCleared();
+			}
+			else
+			{
+				levels[++currentLevel].StartLevel();
+			}
+		}
+	}	
+	
+}
---
+++ Assets/Scripts/SpawnLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class SpawnLevel : MonoBehaviour {
+
+	public List<Pawn> enemies;
+
+	private bool isClear = false;
+
+	public void StartLevel()
+	{
+		foreach(var p in enemies)
+		{
+			p.gameObject.SetActive(true);
+			//Particle 작동시키기 (소환)
+		}
+	}
+	public bool IsClear()
+	{
+		return isClear;
+	}
+
+	void Awake()
+	{
+		foreach(var p in enemies)
+		{
+			p.gameObject.SetActive(false);
+		}
+	}
+
+	void Update()
+	{
+		isClear = enemies.All((p) =>
+		{
+			return p.hp < 0;
+		});
+	}
+}
