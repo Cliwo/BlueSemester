dde6244608dfce8dc1a2fbc293a1d5a827978106
Chan <cliwo@naver.com>
2018-12-05 23:01:53 +0900

    상자 오픈

---
+++ Assets/Scripts/Interaction/InteractiveTreasureBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveTreasureBox : InteractionObject
+{
+	[SerializeField]
+    private Animator animator;
+    public override float InteractingTime
+    {
+        get
+        {
+			return 0.1f;
+        }
+    }
+
+	  protected override void OnInteractionEnd()
+    {
+        base.OnInteractionEnd();
+		animator.SetTrigger("open");
+        inst_Input.InteractionBundles.Remove(bundle);
+    }
+}
