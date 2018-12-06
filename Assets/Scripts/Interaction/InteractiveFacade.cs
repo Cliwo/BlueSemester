abb7c439d63b4abe3a03a41877fd69bb48f60c04
Chan <cliwo@naver.com>
2018-12-06 22:07:07 +0900

    [씬 로드 업데이트] 메인 씬에서 새로운 씬을 로드할 때 규칙이 필요함 1. Character와 Manager를 로드할 씬에서 제거함 2. Character나 Manager를 참조하는 클래스들은 모두 Start이후에 참조해야함 3. CharacterSocket이라는 (정확한 이름을 써야함) 빈 게임오브젝트로 로드 이후 캐릭터의 위치와 회전을 정해 줄 수 있음.

--- Assets/Scripts/Interaction/InteractiveFacade.cs
+++ Assets/Scripts/Interaction/InteractiveFacade.cs
@@ -17,6 +17,13 @@ public class InteractiveFacade : InteractionObject
 	protected override void OnInteractionEnd()
     {
         base.OnInteractionEnd();
-		checkDialog.OpenDialog(CheckDialog.ENTER_DUNGEON, ()=> { SceneManager.LoadScene("Dungeon4"); } );
+        /* 원래 find를 최대한 자제해야함 혹시 시간나면 리팩토링하기 */
+        SceneManager.sceneLoaded += (_, __)=>
+        {
+            GameObject socket = GameObject.Find("CharacterSocket");
+            CharacterManager.getInstance().gameObject.transform.position = socket.transform.position;
+            CharacterManager.getInstance().gameObject.transform.rotation = socket.transform.rotation; 
+        };
+		checkDialog.OpenDialog(CheckDialog.ENTER_DUNGEON, ()=> { SceneManager.LoadScene("Dungeon4_nonDuplication"); } );
     }
 }
