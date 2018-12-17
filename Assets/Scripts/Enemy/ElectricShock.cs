afe76c6e9b9bbd19a47119756a15e0cd3be6a2ad
cocococco <kyeonsoo26@gmail.com>
2018-12-17 13:29:08 +0900

    물웅덩이 감전 구현

---
+++ Assets/Scripts/Enemy/ElectricShock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricShock : MonoBehaviour
+{
+    public float damage = 10;
+    public GameObject effect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter!");
+        Pawn pawnScript = other.gameObject.GetComponent<Pawn>();
+
+        if (pawnScript != null && other.gameObject.tag == "Player")
+        {
+            Debug.Log("player!");
+
+            GameObject go = Instantiate(effect, other.gameObject.transform.position, Quaternion.identity);
+            Destroy(go, 3);
+            ApplyDamage(pawnScript);
+        }
+    }
+
+    private void ApplyDamage(Pawn target)
+    {
+        target.hp -= damage;
+        Debug.Log("Player damaged - " + damage + " = " + target.hp);
+    }
+}
